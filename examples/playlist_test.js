44dd0d2462395391b696e6991deac2b5e11e27ce
gatecrasher777 <gatecrasher777@users.noreply.github.com>
2021-09-08 23:11:42 +0200

    Add files via upload

---
+++ examples/playlist_test.js
@@ -0,0 +1,67 @@
+// ytcog - innertube library - examples to test playlist class
+// (c) 2021 gatecrasher777
+// https://github.com/gatecrasher777/ytcog
+// MIT Licenced
+
+const ytcog = require('../lib/index');
+const ut = require('../lib/ut')();
+const fs = require('fs');
+
+//User editable data:
+let app = {
+    cookie: '',
+    userAgent: '',
+    ignore: ['cookie','userAgent','options','sapisid','status','reason','cancelled','canEmbed','isLive','debugOn'],
+    test_options: {
+        id: 'PLQ_voP4Q3cffatM_zKUO5-woz34KyLrPL', //'J7', //any playlist id
+        quantity: 100 // number of results to fetch. You can get a further 100 more videos (if available) with subsequent playlist.continued() calls
+    }
+}
+
+async function run() {
+    let session = new ytcog.Session(app.cookie,app.userAgent);
+    await session.fetch();
+    console.log(`Session status: ${session.status} (${session.reason})`);
+    if (session.status == 'OK') {
+        let playlist = new ytcog.Playlist(session,app.test_options);
+        playlist.debugOn = true;
+        console.log('\nFetch Playlist videos');
+        await playlist.fetch();
+        console.log(`Playlist status: ${playlist.status} (${playlist.reason})`);
+        if (playlist.status == 'OK') {
+            console.log(`\nLoaded ${playlist.results.length} videos of ${playlist.title}`);
+            console.log('Want some more? Will continue...');
+            await playlist.continued();
+            console.log(`Playlist status: ${playlist.status} (${playlist.reason})`);
+            if (playlist.status == 'OK') {
+                console.log(`\nFound ${playlist.results.length} videos of ${playlist.title}`);
+                console.log('\nPlaylist info/results to ./examples/playlist_results.json');
+                let output = {
+                    playlist: playlist.info(app.ignore),
+                    results : []
+                }
+                playlist.results.forEach((video)=>{
+                    output.results.push(video.info(app.ignore));
+                });
+                fs.writeFileSync('./examples/playlist_results.json',ut.jsp(output),'utf8');
+                console.log('Raw playlist json to ./examples/playlist.json');
+                fs.writeFileSync('./examples/playlist.json',ut.jsp(playlist.data),'utf8');
+            } else {
+                console.log('Raw playlist json to ./examples/playlist.json');
+                fs.writeFileSync('./examples/playlist.json',ut.jsp(playlist.data),'utf8');
+            }
+        } else {
+            console.log('Raw playlist json to ./examples/playlist.json');
+            fs.writeFileSync('./examples/playlist.json',ut.jsp(playlist.data),'utf8');
+        }
+    }
+}
+
+if (process.argv.length==2) {
+    run();
+} else if (process.argv.length==3) {
+    app.test_options.id = process.argv[2];
+    run();
+} else {
+    console.log('usage: >node playlist_test [id]');
+}
--- examples/search_test.js
+++ examples/search_test.js
@@ -13,10 +13,12 @@ let app = {
     userAgent: '',
     test_options: {
         query: 'soccer', //any search term
-        period: 'day', //any,hour,day,week,month,year
+        items: 'any', // any, videos, channels, playlists, movies
         order: 'relevance', // relevance, age, views, rating
-        duration: 'any',	// any, short, long
-        quantity: 100	//number of results to fetch. You can get further results with subsequent search.continued() calls 
+        period: 'week', //any,hour,day,week,month,year 
+        duration: 'any',	// any, short, medium, long
+        quantity: 100,	//number of results to fetch. You can get further results with subsequent search.continued() calls 
+        features: '' // can include one or more comma-separated: live, 4k, hd, subtitles, cc, 360, vr180, 3d, hdr, location, purchased
     }
 }
 
@@ -26,22 +28,58 @@ async function run() {
     console.log(`Session status: ${session.status} (${session.reason})`);
     if (session.status == 'OK') {
         let search = new ytcog.Search(session,app.test_options);
+        search.debugOn = true;
         await search.fetch();
         console.log(`\nSearch status: ${search.status} (${search.reason})`);
         if (search.status == 'OK') {
-            console.log(`Found ${search.videos.length} videos for "${search.options.query}"`);
+            console.log(`Found ${search.results.length} results for "${search.options.query}"`);
             console.log('\nSearch info/results saved to ./examples/search_results.json');
             let output = {
                 search: search.info(['cookie','userAgent','sapisid']),
                 results : []
-            }
-            search.videos.forEach((video)=>{
-                output.results.push(video.info(['cookie','userAgent','options','sapisid','status','reason','cancelled']));
+            };
+            search.results.forEach((item)=>{
+                output.results.push(item.info(['cookie','userAgent','options','sapisid','status','reason','cancelled']));
             });
             fs.writeFileSync('./examples/search_results.json',ut.jsp(output),'utf8');
-            console.log('Search json saved to ./examples/search.json');
+            console.log('Raw search json saved to ./examples/search.json');
             fs.writeFileSync('./examples/search.json',ut.jsp(search.data),'utf8');
+            console.log('\n\nSearch for 50 channels')
+            await search.fetch({items:'channels', quantity: 50});
+            console.log(`\nSearch status: ${search.status} (${search.reason})`);
+            if (search.status == 'OK') {
+                console.log(`Found ${search.results.length} results for "${search.options.query}"`);
+                console.log('\nSearch info/results saved to ./examples/search_channels.json');
+                let output = {
+                    search: search.info(['cookie','userAgent','sapisid']),
+                    results : []
+                };
+                search.results.forEach((item)=>{
+                    output.results.push(item.info(['cookie','userAgent','options','sapisid','status','reason','cancelled']));
+                });
+                fs.writeFileSync('./examples/search_channels.json',ut.jsp(output),'utf8');
+                console.log('Raw search json saved to ./examples/searchC.json');
+                fs.writeFileSync('./examples/searchC.json',ut.jsp(search.data),'utf8');
+                console.log('\n\nSearch for 30 playlists')
+                await search.fetch({items:'playlists', quantity: 30});
+                console.log(`\nSearch status: ${search.status} (${search.reason})`);
+                if (search.status == 'OK') {
+                    console.log(`Found ${search.results.length} results for "${search.options.query}"`);
+                    console.log('\nSearch info/results saved to ./examples/search_playlists.json');
+                    let output = {
+                        search: search.info(['cookie','userAgent','sapisid']),
+                        results : []
+                    };
+                    search.results.forEach((item)=>{
+                        output.results.push(item.info(['cookie','userAgent','options','sapisid','status','reason','cancelled']));
+                    });
+                    fs.writeFileSync('./examples/search_playlists.json',ut.jsp(output),'utf8');
+                    console.log('Raw search json saved to ./examples/searchP.json');
+                    fs.writeFileSync('./examples/searchP.json',ut.jsp(search.data),'utf8');
+                }
+            }
         }
+
     }
 }
 
