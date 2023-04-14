430e23243ea7011f9198f10307993598311a2879
gatecrasher777 <gatecrasher777@users.noreply.github.com>
2023-04-14 18:11:51 +0200

    Add files via upload

--- examples/channel_test.js
+++ examples/channel_test.js
@@ -16,20 +16,148 @@ let app = {
 	// proxy agent string
 	proxy: '',
 	// info fields to ignore
-	ignore: ['cookie', 'userAgent', 'options', 'sapisid', 'status', 'reason', 'cancelled'],
+	ignore: ['cookie', 'userAgent', 'options', 'sapisid', 'status', 'reason', 'cancelled', 'debugOn','commentOptions','commentOrder','canEmbed','isLive'],
 	test_options: {
 		// any channel id
 		id: 'UCG5qGWdu8nIRZqJ_GgDwQ-w',
-		// videos, playlists, channels, about, search
+		// videos, shorts, playlists, channels, about, search
 		items: 'about',
 		// new (videos/playlists), old (videos), views (videos), updated (playlists)
 		order: 'new',
 		// number of results to fetch. You can get further videos (if available) with subsequent channel.continued() calls
-		quantity: 60,
+		quantity: 30,
 		// items=search only
-		query: '',
+		query: 'after:'+ new Date(ut.now()-1000*60*60*24*7).toISOString().substring(0,10),
 	},
 };
+
+async function runAbout(channel) {
+	console.log('\nFetch channel profile data (items: about)');
+	await channel.fetch();
+	console.log(`Channel status: ${channel.status} (${channel.reason})`);
+	console.log('Raw Channel json to ./examples/channelAbout.json');
+	fs.writeFileSync('./examples/channelAbout.json', ut.jsp(channel.data), 'utf8');
+	if (channel.status === 'OK') {
+		console.log(`\nChannel info for ${channel.author} to ./examples/channel_about.json`);
+		fs.writeFileSync('./examples/channel_about.json', ut.jsp(channel.info(app.ignore)), 'utf8');
+	}
+}
+
+async function runVideos(channel) {
+	console.log('\nFetch latest Channel videos');
+	await channel.fetch({ items: 'videos' });
+	console.log(`Channel status: ${channel.status} (${channel.reason})`);
+	console.log('Raw Channel json to ./examples/channelVideos.json');
+	fs.writeFileSync('./examples/channelVideos.json', ut.jsp(channel.data), 'utf8');
+	if (channel.status === 'OK') {
+		console.log(`\nFound ${channel.results.length} results for ${channel.author}`);
+		console.log('Want some more? Will continue...');
+		await channel.continued();
+		fs.writeFileSync('./examples/channelVideos.json', ut.jsp(channel.data), 'utf8');
+		console.log(`Channel status: ${channel.status} (${channel.reason})`);
+		if (channel.status === 'OK') {
+			console.log(`\nFound ${channel.results.length} videos for ${channel.author}`);
+			console.log('\nChannel info/videos to ./examples/channel_videos.json');
+			let output = {
+				channel: channel.info(app.ignore),
+				videos: [],
+			};
+			channel.results.forEach(video => {
+				output.videos.push(video.info(app.ignore));
+			});
+			fs.writeFileSync('./examples/channel_videos.json', ut.jsp(output), 'utf8');
+		}
+	}
+}
+
+async function runShorts(channel) {
+	console.log('\nFetch latest Channel shorts');
+	await channel.fetch({ items: 'shorts' });
+	console.log(`Channel status: ${channel.status} (${channel.reason})`);
+	console.log('Raw Channel json to ./examples/channelShorts.json');
+	fs.writeFileSync('./examples/channelShorts.json', ut.jsp(channel.data), 'utf8');
+	if (channel.status === 'OK') {
+		console.log(`\nFound ${channel.results.length} results for ${channel.author}`);
+		console.log('Want some more? Will continue...');
+		await channel.continued();
+		fs.writeFileSync('./examples/channelShorts.json', ut.jsp(channel.data), 'utf8');
+		console.log(`Channel status: ${channel.status} (${channel.reason})`);
+		if (channel.status === 'OK') {
+			console.log(`\nFound ${channel.results.length} shorts for ${channel.author}`);
+			console.log('\nChannel info/shorts to ./examples/channel_shorts.json');
+			let output = {
+				channel: channel.info(app.ignore),
+				shorts: [],
+			};
+			channel.results.forEach(video => {
+				output.shorts.push(video.info(app.ignore));
+			});
+			fs.writeFileSync('./examples/channel_shorts.json', ut.jsp(output), 'utf8');
+		}
+	}
+}
+
+async function runPlaylists(channel) {
+	console.log('\n\nFetch 30 channel playlists (most recently updated)...');
+	await channel.fetch({ items: 'playlists', order: 'updated', quantity: 30 });
+	console.log('Raw Channel json to ./examples/channelPlaylists.json');
+	fs.writeFileSync('./examples/channelPlaylists.json', ut.jsp(channel.data), 'utf8');
+	console.log(`Channel status: ${channel.status} (${channel.reason})`);
+	if (channel.status === 'OK') {
+		console.log(`\nFound ${channel.results.length} playlist results for ${channel.author}`);
+		console.log('\nChannel info/playlists to ./examples/channel_playlists.json');
+		let output = {
+			channel: channel.info(app.ignore),
+			playlists: [],
+		};
+		channel.results.forEach(item => {
+			output.playlists.push(item.info(app.ignore));
+		});
+		fs.writeFileSync('./examples/channel_playlists.json', ut.jsp(output), 'utf8');
+	}
+}
+
+async function runChannels(channel) {
+	console.log('\n\nFetch 20 related channels...');
+	await channel.fetch({ items: 'channels', order: 'new', quantity: 20 });
+	console.log('Raw Channel json to ./examples/channelChannels.json');
+	fs.writeFileSync('./examples/channelChannels.json', ut.jsp(channel.data), 'utf8');
+	console.log(`Channel status: ${channel.status} (${channel.reason})`);
+	if (channel.status === 'OK') {
+		console.log(`\nFound ${channel.results.length} related channels for ${channel.author}`);
+		console.log('\nChannel info/channels to ./examples/channel_channels.json');
+		let output = {
+			channel: channel.info(app.ignore),
+			channels: [],
+		};
+		channel.results.forEach(item => {
+			output.channels.push(item.info(app.ignore));
+		});
+		fs.writeFileSync('./examples/channel_channels.json', ut.jsp(output), 'utf8');
+
+	}
+}
+
+async function runSearch(channel) {
+	console.log(`\n\nSearch for up to 100 videos/shorts in the channel with query: "${app.test_options.query}"`);
+	await channel.fetch({ items: 'search',	quantity: 100 });
+	console.log('Raw Channel json to ./examples/channelSearch.json');
+	fs.writeFileSync('./examples/channelSearch.json', ut.jsp(channel.data), 'utf8');
+	console.log(`Channel status: ${channel.status} (${channel.reason})`);
+	if (channel.status === 'OK') {
+		console.log(`\nFound ${channel.results.length} search results in ${channel
+			.author} for "${channel.options.query}"`);
+		console.log('\nChannel search info/videos to ./examples/channel_search.json');
+		let output = {
+			channel: channel.info(app.ignore),
+			videos: [],
+		};
+		channel.results.forEach(item => {
+			output.videos.push(item.info(app.ignore));
+		});
+		fs.writeFileSync('./examples/channel_search.json', ut.jsp(output), 'utf8');
+	}
+}
 
 async function run() {
 	let session = new ytcog.Session(app.cookie, app.userAgent, app.proxy);
@@ -38,106 +166,15 @@ async function run() {
 	if (session.status === 'OK') {
 		let channel = new ytcog.Channel(session, app.test_options);
 		channel.debugOn = true;
-		console.log('\nFetch channel profile data (items: about)');
-		await channel.fetch();
-		console.log('Raw Channel json to ./examples/channelA.json');
-		fs.writeFileSync('./examples/channelA.json', ut.jsp(channel.data), 'utf8');
-		console.log(`Channel status: ${channel.status} (${channel.reason})`);
-		if (channel.status === 'OK') {
-			console.log('\nFetch latest Channel videos');
-			await channel.fetch({ items: 'videos' });
-			console.log(`Channel status: ${channel.status} (${channel.reason})`);
-			if (channel.status === 'OK') {
-				console.log(`\nFound ${channel.results.length} results for ${channel.author}`);
-				console.log('Want some more? Will continue...');
-				await channel.continued();
-				console.log(`Channel status: ${channel.status} (${channel.reason})`);
-				if (channel.status === 'OK') {
-					console.log(`\nFound ${channel.results.length} video results for ${channel.author}`);
-					console.log('\nChannel info/results to ./examples/channel_results.json');
-					let output1 = {
-						channel: channel.info(app.ignore),
-						results: [],
-					};
-					channel.results.forEach(video => {
-						output1.results.push(video.info(app.ignore));
-					});
-					fs.writeFileSync('./examples/channel_results.json', ut.jsp(output1), 'utf8');
-					console.log('Raw Channel json to ./examples/channel.json');
-					fs.writeFileSync('./examples/channel.json', ut.jsp(channel.data), 'utf8');
-					console.log('\n\nFetch 30 channel playlists (most recently updated)...');
-					await channel.fetch({ items: 'playlists', order: 'updated', quantity: 30 });
-					console.log(`Channel status: ${channel.status} (${channel.reason})`);
-					if (channel.status === 'OK') {
-						console.log(`\nFound ${channel.results.length} playlist results for ${channel.author}`);
-						console.log('\nChannel info/results to ./examples/channel_playlists.json');
-						let output2 = {
-							channel: channel.info(app.ignore),
-							results: [],
-						};
-						channel.results.forEach(item => {
-							output2.results.push(item.info(app.ignore));
-						});
-						fs.writeFileSync('./examples/channel_playlists.json', ut.jsp(output2), 'utf8');
-						console.log('Raw Channel json to ./examples/channelP.json');
-						fs.writeFileSync('./examples/channelP.json', ut.jsp(channel.data), 'utf8');
-						console.log('\n\nFetch 20 related channels...');
-						await channel.fetch({ items: 'channels', order: 'new', quantity: 20 });
-						console.log(`Channel status: ${channel.status} (${channel.reason})`);
-						if (channel.status === 'OK') {
-							console.log(`\nFound ${channel.results.length} channel results for ${channel.author}`);
-							console.log('\nChannel info/results to ./examples/channel_channels.json');
-							let output3 = {
-								channel: channel.info(app.ignore),
-								results: [],
-							};
-							channel.results.forEach(item => {
-								output3.results.push(item.info(app.ignore));
-							});
-							fs.writeFileSync('./examples/channel_channels.json', ut.jsp(output3), 'utf8');
-							console.log('Raw Channel json to ./examples/channelC.json');
-							fs.writeFileSync('./examples/channelC.json', ut.jsp(channel.data), 'utf8');
-							console.log('\n\nSearch for 20 videos in the channel ...');
-							await channel.fetch({ items: 'search', query: 'Chelsea', quantity: 20 });
-							console.log(`Channel status: ${channel.status} (${channel.reason})`);
-							if (channel.status === 'OK') {
-								console.log(`\nFound ${channel.results.length} search results in ${channel
-									.author} for ${channel.options.query}`);
-								console.log('\nChannel info/results to ./examples/channel_search.json');
-								let output4 = {
-									channel: channel.info(app.ignore),
-									results: [],
-								};
-								channel.results.forEach(item => {
-									output4.results.push(item.info(app.ignore));
-								});
-								fs.writeFileSync('./examples/channel_search.json', ut.jsp(output4), 'utf8');
-								console.log('Raw Channel json to ./examples/channelS.json');
-								fs.writeFileSync('./examples/channelS.json', ut.jsp(channel.data), 'utf8');
-							} else {
-								console.log('Raw Channel json to ./examples/channelS.json');
-								fs.writeFileSync('./examples/channelS.json', ut.jsp(channel.data), 'utf8');
-							}
-						} else {
-							console.log('Raw Channel json to ./examples/channelC.json');
-							fs.writeFileSync('./examples/channelC.json', ut.jsp(channel.data), 'utf8');
-						}
-					} else {
-						console.log('Raw Channel json to ./examples/channelP.json');
-						fs.writeFileSync('./examples/channelP.json', ut.jsp(channel.data), 'utf8');
-					}
-				} else {
-					console.log('Raw Channel json to ./examples/channel.json');
-					fs.writeFileSync('./examples/channel.json', ut.jsp(channel.data), 'utf8');
-				}
-			} else {
-				console.log('Raw Channel json to ./examples/channel.json');
-				fs.writeFileSync('./examples/channel.json', ut.jsp(channel.data), 'utf8');
-			}
-		} else {
-			console.log('Raw Channel json to ./examples/channelA.json');
-			fs.writeFileSync('./examples/channelA.json', ut.jsp(channel.data), 'utf8');
-		}
+		await runAbout(channel);
+		await runVideos(channel);
+		await runShorts(channel);
+		await runPlaylists(channel);
+		await runChannels(channel);
+		await runSearch(channel);
+		console.log('Session complete.')
+	} else {
+		console.log('Session failed.')
 	}
 }
 
--- examples/video_test.js
+++ examples/video_test.js
@@ -17,7 +17,7 @@ let app = {
 	// proxy agent string
 	proxy: '',
 	// info fields to ignore
-	ignore: ['cookie', 'userAgent', 'options', 'sapisid', 'status', 'reason', 'cancelled'],
+	ignore: ['cookie', 'userAgent', 'options', 'sapisid', 'status', 'reason', 'cancelled','debugOn','commentOptions','commentOrder','defaultCaptions','formnats'],
 	// video options
 	test_options: {
 		// any video id
@@ -29,7 +29,7 @@ let app = {
 		// supply a optional filename, with placeholders, but do not include an extension.
 		filename: '${author}_${datetime}_${title}_${id}_${videoQuality}_${videoCodec}_${audioCodec}',
 		// any, mp4, webm, mkv
-		container: 'any',
+		container: 'mkv',
 		// desired quality: highest, 1080p, 720p, 480p, medium, 360p, 240p, 144p, lowest
 		videoQuality: '1080p',
 		// desired audio quality: highest, medium, lowest
@@ -42,8 +42,10 @@ let app = {
 		audioFormat: -1,
 		// Metadata to add to downloaded media
 		metadata: 'author,title,published',
-		// make srt subtitles if captions are available, chose language codes, comma separated
+		// make subtitles if captions are available, chose language codes, comma separated
 		subtitles: 'en,es,ja',
+		// chose the subtitle file format, one or more of srt, vtt or ttml
+		subtitleFormat: 'srt,vtt,ttml',
 		// supply a callback for download progress;
 		progress: (prg, siz, tot) => {
 			app.downloaded += siz;
@@ -60,67 +62,92 @@ let app = {
 	},
 };
 
+async function runOnline(video) {
+	console.log('\nCheck online status via preview image');
+	await video.imageOnline();
+	console.log(`Video status: ${video.status} (${video.reason})`);
+}
+
+async function runInfo(video) {
+	console.log('\nFetch video metadata and streams');
+	await video.fetch();
+	console.log(`Video status: ${video.status} (${video.reason})`);
+	console.log('\nVideo info saved to ./examples/video_info.json');
+	let output = video.info(app.ignore);
+	fs.writeFileSync('./examples/video_info.json', ut.jsp(output), 'utf8');
+	console.log('Video json to ./examples/video.json');
+	fs.writeFileSync('./examples/video.json', ut.jsp(video.data), 'utf8');
+	if (video.status === 'OK') {
+		console.log('\nAvailable media streams:');
+		console.log(video.streamInfo);
+		console.log(`\nStreams expire in ${ut.secDur(video.timeToExpiry, 'hms')}`);
+		console.log(video.captions);
+	}
+}
+
+async function runComments(video) {
+	console.log('\nDownloading comments (if available)');
+	if (await video.hasComments()) {
+		console.log(`Number of Comments: ${video.commentCount}`);
+		await video.commentsText(app.comment_options);
+		console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
+		if (video.status === 'OK') console.log(`Success - comments saved to ${video.cfn}`);
+	} else {
+		console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
+	}
+	console.log('\nVideo comments json saved to ./examples/videoComments.json');
+	fs.writeFileSync('./examples/videoComments.json', ut.jsp(video.commentData), 'utf8');
+}
+
+async function runDownloads(video) {
+	console.log(`\nDownloading test video using given test options`);
+	await video.download();
+	console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
+	if (video.downloaded) {
+		console.log(`Success - video saved to ${video.fn}`);
+		console.log(`\nDownloading video using video stream 3 and audio stream 1 (mp4 only)`);
+		app.downloaded = 0;
+		await video.download({ videoFormat: 3, audioFormat: 1 });
+		console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
+		if (video.downloaded) {
+			console.log(`Success - custom video saved to ${video.fn}`);
+			console.log(`\nDownloading video only`);
+			video.updateOptions(app.test_options);
+			app.downloaded = 0;
+			await video.download({ audioQuality: 'none' });
+			console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
+			if (video.downloaded) {
+				console.log(`Success - video only saved to ${video.fn}`);
+				console.log(`\nDownloading audio only`);
+				video.updateOptions(app.test_options);
+				app.downloaded = 0;
+				await video.download({ videoQuality: 'none', container: 'mp3' });
+				console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
+				if (video.downloaded) {
+					console.log(`Success - audio only (mp3) saved to ${video.fn}`);
+				}
+			}
+		}
+	}
+}
+
 async function run() {
 	let session = new ytcog.Session(app.cookie, app.userAgent, app.proxy);
 	await session.fetch();
 	console.log(`Session status: ${session.status} (${session.reason})`);
+	if (session.loggedIn) {
+		console.log('\nYour are logged into YouTube. Enjoy.');
+	} else {
+		console.log('\nYou are not logged into YouTube. As a consequence:' +
+			' \nSome downloads in ytcog may not work \nYou may experience rate limiting and age-restrictions.');
+	}
 	if (session.status === 'OK') {
 		let video = new ytcog.Video(session, app.test_options);
 		video.debugOn = true;
-		console.log('\nFetch video metadata and streams');
-		await video.fetch();
-		console.log(`Video status: ${video.status} (${video.reason})`);
-		if (video.status === 'OK') {
-			console.log('\nVideo info saved to ./examples/video_info.json');
-			let output = video.info(app.ignore);
-			fs.writeFileSync('./examples/video_info.json', ut.jsp(output), 'utf8');
-			console.log('Video json to ./examples/video.json');
-			fs.writeFileSync('./examples/video.json', ut.jsp(video.data), 'utf8');
-			console.log('\nAvailable media streams:');
-			console.log(video.streamInfo);
-			console.log(`\nStreams expire in ${ut.secDur(video.timeToExpiry, 'hms')}`);
-			console.log(video.captions);
-			console.log('\nDownloading comments (if available)');
-			if (await video.hasComments()) {
-				console.log(`Number of Comments: ${video.commentCount}`);
-				await video.commentsText(app.comment_options);
-				console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
-				if (video.status === 'OK') console.log(`Success - comments saved to ${video.cfn}`);
-			} else {
-				console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
-			}
-			console.log('\nVideo comments json saved to ./examples/videoComments.json');
-			fs.writeFileSync('./examples/videoComments.json', ut.jsp(video.commentData), 'utf8');
-			console.log(`\nDownloading test video using given test options`);
-			await video.download();
-			console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
-			if (video.downloaded) {
-				console.log(`Success - video saved to ${video.fn}`);
-				console.log(`\nDownloading video using video stream 3 and audio stream 1 (mp4 only)`);
-				app.downloaded = 0;
-				await video.download({ videoFormat: 3, audioFormat: 1 });
-				console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
-				if (video.downloaded) {
-					console.log(`Success - custom video saved to ${video.fn}`);
-					console.log(`\nDownloading video only`);
-					video.updateOptions(app.test_options);
-					app.downloaded = 0;
-					await video.download({ audioQuality: 'none' });
-					console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
-					if (video.downloaded) {
-						console.log(`Success - video only saved to ${video.fn}`);
-						console.log(`\nDownloading audio only`);
-						video.updateOptions(app.test_options);
-						app.downloaded = 0;
-						await video.download({ videoQuality: 'none', container: 'mp3' });
-						console.log(`\n\nVideo status: ${video.status} (${video.reason})`);
-						if (video.downloaded) {
-							console.log(`Success - audio only (mp3) saved to ${video.fn}`);
-						}
-					}
-				}
-			}
-		}
+		runOnline(video);
+		runInfo(video);
+		runComments(video);
+		runDownloads(video);
 	}
 }
 
